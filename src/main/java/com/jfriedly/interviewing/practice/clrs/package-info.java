aac353c3264a2379c92d448de73fd6418b32d50e
Joel Friedly <joelfriedly@gmail.com>
2020-01-01 18:27:42 -0800

    Add package-info.java files explaining where the problems come from

---
+++ src/main/java/com/jfriedly/interviewing/practice/clrs/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Interview practice problems drawn from Introduction to Algorithms by Cormen, Leiserson, Rivest, and Stein (CLRS).
+ */
+package com.jfriedly.interviewing.practice.clrs;
---
+++ src/main/java/com/jfriedly/interviewing/practice/tadm2e/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Interview practice problems drawn from The Algorithm Design Manual, 2nd Edition (TADM2E) by Skiena.
+ */
+package com.jfriedly.interviewing.practice.tadm2e;
