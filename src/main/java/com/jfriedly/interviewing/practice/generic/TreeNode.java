c25fd8449a0fc27050de71e3d2e2e17f4af0d89c
Joel Friedly <joelfriedly@gmail.com>
2020-01-06 17:15:11 -0800

    Add a reusable TreeNode class as well

---
+++ src/main/java/com/jfriedly/interviewing/practice/generic/TreeNode.java
@@ -0,0 +1,68 @@
+package com.jfriedly.interviewing.practice.generic;
+
+import java.util.Objects;
+
+public class TreeNode{
+
+    private Integer value;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(Integer value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(Integer value) {
+        this(value, null, null);
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return Objects.equals(value, treeNode.value) &&
+                Objects.equals(left, treeNode.left) &&
+                Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, left, right);
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "value=" + value +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
